a495d8bf5debcfa0e484ca477f3f18fde381d893
flizcano <flizcanor@icloud.com>
2023-02-12 18:30:30 -0500

    Ajuste texto de actividad didactica

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -2,7 +2,7 @@
 .curso-main-container.pb-3
   BannerInterno(icono="fas fa-tasks" titulo="Actividad didáctica")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
-    
+
     .row.mb-5.justify-content-center.align-items-center.align-items-lg-stretch
       .col-6.col-md-4.col-lg-3.mb-4.mb-md-0
         .tarjeta.h-100.d-flex.align-items-center.p-4
@@ -11,9 +11,9 @@
       .col-12.col-md-8.col-lg-9
         .titulo-segundo
           h2 Diseño del programa de capacitación de recurso humano
-        p.mb-4 Apreciado aprendiz, a continuación, encontrará una serie de preguntas que deberá resolver, con el objetivo de evaluar la aprehensión de los conocimientos expuestos en este componente formativo.
+        p.mb-4 Apreciado aprendiz, a continuación, encontrará una serie de preguntas que deberá resolver, con el objetivo de evaluar la aprehensión de los conocimientos expuestos en este componente formativo:
         .tarjeta.actividad.p-3
-          .row.justify-content-around.align-items-center            
+          .row.justify-content-around.align-items-center
             .col-sm.mb-3.mb-sm-0
               p.fw-bold.mb-0 Falso y verdadero
             .col-auto
